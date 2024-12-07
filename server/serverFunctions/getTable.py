488c3d34f54c2ca2c0ce4f90d303bb9957429f34
YLaret <yannicklaret@gmail.com>
2024-12-07 14:05:29 +0100

    fixed the close position really now

--- server/serverFunctions/getTable.py
+++ server/serverFunctions/getTable.py
@@ -5,14 +5,15 @@ def getTable(table):
     # connect to database
     db = sqlite3.connect('../data/machine.db',timeout=5)
 
-    # get the names of the columns and close the connection
+    # get the names of the columns
     cs = db.execute('SELECT * FROM ' + table)
     ns = [description[0] for description in cs.description]
-    db.close()
 
     # turn the table into a dict list
     result = []
     for ci in cs:
         result.append(dict(zip(ns,ci)))
 
+    # close connection
+    db.close()
     return result
