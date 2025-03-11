6c449d908eccd5592092d7ac1f7f43d5396239e2
YLaret <yannicklaret@gmail.com>
2025-03-11 23:07:59 +0000

    another backup push

--- server/serverFunctions/stop.py
+++ server/serverFunctions/stop.py
@@ -5,6 +5,7 @@ def stop():
     db = sqlite3.connect('../data/machine.db',timeout=5)
     db.execute('UPDATE MACHINESTATUS SET Pause = 2')
     db.execute('UPDATE MACHINESTATUS SET ProgramRunTime = 0')
+    db.execute('UPDATE STAGE SET SwitchIDS = "0" WHERE StageID = 1')
     db.commit()
     # close connection
     db.close()
