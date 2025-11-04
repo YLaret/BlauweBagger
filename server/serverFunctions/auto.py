0206f979d95f65df380ac9845225e6be684e010e
YLaret <yannicklaret@gmail.com>
2025-11-04 14:25:50 +0000

    database rule correction, init auto with no activeSw

--- server/serverFunctions/auto.py
+++ server/serverFunctions/auto.py
@@ -4,6 +4,7 @@ def auto():
     # connect to database
     db = sqlite3.connect('../data/machine.db',timeout=5)
     db.execute('UPDATE MACHINESTATUS SET Pause = 14')
+    db.execute('UPDATE STAGE SET SwitchIDS = "0" WHERE StageID = 2')
     db.commit()
     # close connection
     db.close()
