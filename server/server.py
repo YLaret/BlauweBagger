b202e15a9aea488ce6d6ad911d70b4450ded2993
Yannick Laret <yannicklaret@Yannicks-MacBook-Air.local>
2026-07-17 15:46:16 +0200

    500 test

--- server/server.py
+++ server/server.py
@@ -91,7 +91,7 @@ def updatePage():
     
     # process meter reading
     for meter in meterData:
-        meters.append(round(float(meter["Value"]),3))
+        meters.append(round(float(meter["Value"])))
     
     # aggrate meter values
     aM = sF.aggrateMeters(meterData)
